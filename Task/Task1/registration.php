3239dd954c90ff7abd1c688d1b25e68f3271fbd4
AbdulNishar05 <80945971+AbdulNishar05@users.noreply.github.com>
2021-09-06 18:27:50 +0530

    Add files via upload

---
+++ Task/Task1/registration.php
@@ -0,0 +1,8 @@
+<?php
+  \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Task_Task1',
+    __DIR__
+);
+
+?>
---
+++ Task/Task2/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Task_Task2',
+    __DIR__
+);
